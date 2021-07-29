b371ec121d6b42eeccf337fd88fdbaed0d791411
Rene <renealejandrosanchezmorales@gmail.com>
2021-07-29 18:55:51 -0400

    added feature constructor work

---
+++ constructor.js
@@ -0,0 +1,11 @@
+class MiClase {
+    constructor() {
+        this.propiedad = 1
+        this.metodo = () => {
+            // soy un metodo
+        }
+    }
+}
+
+const instancia = new MiClase()
+console.log(instancia)
