9ad5e3353fa410b28d8569115151efff7127853c
Rene <renealejandrosanchezmorales@gmail.com>
2021-07-29 19:13:57 -0400

    added examples

--- constructor.js
+++ constructor.js
@@ -12,4 +12,32 @@ class MiClase {
 }
 
 const instancia = new MiClase()
-console.log(instancia)
+// console.log(instancia)
+
+//Examples
+
+// example1
+// Hace que todos los tipos objectos en su prototype se le integre el metodo log para ser utilizado
+Object.prototype.log = function () {
+    console.log(this);
+}
+
+// Esto es igual al hacer un new MiClase pero abreviado y menos legible como la clase
+const x = { a: 1 }
+x.log()
+
+// example 2
+
+String.prototype.trim = undefined
+if (!String.prototype.trim) {
+    String.prototype.trim = function () {
+        try {
+            return this.replace(/^\s+|\s+$/g, '')
+        } catch (error) {
+            throw new Error('Cannot delete blank spaces in string')
+        }
+    }
+}
+
+const text = "    lalala    ".trim()
+text.log()
